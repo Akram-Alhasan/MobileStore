d267be8173bbf84de1c24a5576bda289aeaf79fb
akram alhasan <akramalhasan1996@hotmail.com>
2021-08-16 20:08:30 +0300

    Add Value To CreatedAt In All Entity

--- src/BuildingBlocks/Shared/MobileStore.BuildingBlocks.Shared.Shared/EntityParent/EntityParent.cs
+++ src/BuildingBlocks/Shared/MobileStore.BuildingBlocks.Shared.Shared/EntityParent/EntityParent.cs
@@ -7,8 +7,8 @@ namespace MobileStore.BuildingBlocks.Shared.Shared.EntityParent
     public class EntityParent
     {
         public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now; 
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
---
+++ src/Services/Catalog/Infrastructure/MobileStore.Services.Catalog.Infrastructure/Migrations/20210816170644_Edit-UpdatedAt-To-AllowNull.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MobileStore.Services.Catalog.Infrastructure.Migrations
+{
+    public partial class EditUpdatedAtToAllowNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Mobiles",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "MobileAttributes",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Categories",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Mobiles",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "MobileAttributes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
